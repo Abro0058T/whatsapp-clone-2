957c9a91c8276e5982e060ca9345eaaec14ebe61
AbhiAtiUttam <Abhi@glocalize.net>
2024-08-18 14:23:50 +0530

    added voice all and video call feature

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env:{
+    NEXT_PUBLIC_ZEGO_APP_ID:1641008969,
+    NEXT_PUBLIC_ZEGO_SERVER_ID:"f72b270b8d0d3b9d502efed996d05e30"
+  },
   images:{
     domains:["localhost"],
   }
