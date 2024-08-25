978bdb5d92bb647388328180734846c087441a95
AbhiAtiUttam <Abhi@glocalize.net>
2024-08-25 22:52:32 +0530

    completed project and added docker file

--- src/components/Chatlist/ChatListHeader.jsx
+++ src/components/Chatlist/ChatListHeader.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "../common/Avatar";
 import { useStateProvider } from "@/context/StateContext";
 import {BsFillChatLeftTextFill,BsThreeDotsVertical} from 'react-icons/bs';
 import { reducerCases } from "@/context/constants";
+import ContextMenu from "../common/ContextMenu";
+import { useRouter } from "next/router";
 
 function ChatListHeader() {
   const [{userInfo},dispatch]=useStateProvider()
+  const router= useRouter()
+  const [contextMenuCordinates, setContextMenuCordinates] = useState({
+    x:0,
+    y:0
+  });
+  const [isContextMenuVisible, setIsContextMenuVisible] = useState(false);
+
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setContextMenuCordinates({x:e.pageX - 50, y:e.pageY +20});
+    setIsContextMenuVisible(true);
+  }
+
+  const contextMenuOption = [
+    {
+      name: "Logout",
+      callBack: async () =>{
+        // dispatch({type:reducerCases.SET_EXIT_CHAT})
+        setIsContextMenuVisible(false)
+        router.push("/logout")
+      }
+    }
+  ]
+
+
+
 
   const handleAllContactsPage = () => {
     dispatch({type: reducerCases.SET_ALL_CONTACTS_PAGE})
@@ -23,10 +51,21 @@ function ChatListHeader() {
       />
       <>
       <BsThreeDotsVertical 
-      
+      onClick={(e)=> showContextMenu(e)}
       className="text-panel-header-icon  cursor-pointer text-xl"
+      id="context-opener"
       titel="Menu"/>
       </>
+      {
+        isContextMenuVisible && (
+          <ContextMenu
+          options={contextMenuOption}
+          cordinates={contextMenuCordinates}
+          contextMenu={isContextMenuVisible}
+          setContestMenu={setIsContextMenuVisible}
+          />
+        )
+      }
     </div>
   </div>;
 }
